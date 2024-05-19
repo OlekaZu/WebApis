4064c55f9b1bd5282eb04337bcc48f9c50bf7c6d
Dtammie <dtammie@student.21-school.ru>
2024-05-19 15:28:09 +0700

    Add repository

---
+++ ToDoListApi/Data/TaskItemRepository.cs
@@ -0,0 +1,98 @@
+﻿namespace ToDoListApi.Data
+{
+    public class TaskItemRepository : IRepository<TaskItem>
+    {
+        private readonly TasksDb _context;
+
+        public TaskItemRepository(TasksDb context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<TaskItem>> GetAllAsync()
+            => await _context.TaskItems
+            .Include(i => i.Doer)
+            .Include(i => i.Group)
+            .ToListAsync();
+
+        public async Task<List<TaskItem>> GetByIdAsync(int userId)
+            => await _context.TaskItems.Where(i => i.Doer != null && i.Doer.Id == userId)
+            .ToListAsync();
+
+        public async Task<TaskItem?> GetBySpecifiedIdAsync(int userId, int specifiedId)
+        {
+            var list = await GetByIdAsync(userId);
+            if (list.Count <= specifiedId)
+                return null;
+            else
+                return list[specifiedId - 1];
+        }
+
+        public async Task<bool> InsertAsync(TaskItem entity)
+        {
+            var check = await _context.TaskItems.AnyAsync(u => u.Id == entity.Id);
+            if (!check)
+                await _context.TaskItems.AddAsync(entity);
+            return check;
+        }
+
+        public async Task<bool> UpdateAsync(TaskItem entity)
+        {
+            var taskItemFromDb = await _context.TaskItems.FindAsync(new object[] { entity.Id });
+            if (taskItemFromDb == null)
+                return false;
+            taskItemFromDb.Name = entity.Name;
+            taskItemFromDb.Description = entity.Description;
+            taskItemFromDb.DoerId = entity.DoerId;
+            taskItemFromDb.TaskGroupId = entity.TaskGroupId;
+            taskItemFromDb.Begin = entity.Begin;
+            taskItemFromDb.End = entity.End;
+            taskItemFromDb.IsCompleted = entity.IsCompleted;
+            taskItemFromDb.Priority = entity.Priority;
+            return true;
+        }
+
+        public async Task<bool> DeleteByIdAsync(int userId)
+        {
+            var taskItemsByUserId = await _context.TaskItems
+                .Where(x => x.Id == userId).ToListAsync();
+            if (taskItemsByUserId.Count == 0)
+                return false;
+            _context.TaskItems.RemoveRange(taskItemsByUserId);
+            return true;
+        }
+
+        public async Task<bool> DeleteBySpecifiedIdAsync(int userId, int specifiedId)
+        {
+            var taskItemsByUserId = await GetByIdAsync(userId);
+            if (taskItemsByUserId.Count <= specifiedId)
+                return false;
+            _context.TaskItems.Remove(taskItemsByUserId[specifiedId - 1]);
+            return true;
+        }
+
+        public async Task SaveAsync() => await _context.SaveChangesAsync();
+
+        private bool _disposed = false;
+
+        // disposing == false (runtime calls the method and only unmanaged resources are disposed)
+        // disposing == true (user's code calls the method and both managed and unmanaged resources are disposed)
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
--- ToDoListApi/Program.cs
+++ ToDoListApi/Program.cs
@@ -5,6 +5,9 @@ builder.Services.AddDbContext<TasksDb>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite"));
 });
+builder.Services.AddScoped<IRepository<User>, UserRepository>();
+builder.Services.AddScoped<IRepository<TaskGroup>, TaskGroupRepository>();
+builder.Services.AddScoped<IRepository<TaskItem>, TaskItemRepository>();
 
 var app = builder.Build();
 
@@ -15,127 +18,116 @@ if (app.Environment.IsDevelopment())
     db.Database.EnsureCreated();
 }
 
-app.MapGet("/users", async (TasksDb db) => await db.Users.ToListAsync());
-app.MapPost("/users", async ([FromBody] User user, TasksDb db)
+// Users Api
+app.MapGet("/users", async (IRepository<User> repository)
+    => Results.Ok(await repository.GetAllAsync()));
+
+app.MapGet("/users/{id}", async (IRepository<User> repository, int id)
+    => Results.Ok(await repository.GetByIdAsync(id)));
+
+app.MapPost("/users", async (IRepository<User> repository, [FromBody] User user)
     =>
 {
-    if (db.Users.Any(u => u.Id == user.Id || u.UserName == user.UserName))
-        throw new Exception("User already exists");
-    db.Users.Add(user);
-    await db.SaveChangesAsync();
+    if (await repository.InsertAsync(user) == false)
+        return Results.BadRequest();
+    await repository.SaveAsync();
     return Results.Created($"/users/{user.Id}", user);
 });
-app.MapPut("/users", async ([FromBody] User user, TasksDb db) =>
+
+app.MapPut("/users", async (IRepository<User> repository, [FromBody] User user) =>
 {
-    var userFromDb = await db.Users.FindAsync(new object[] { user.Id });
-    if (userFromDb == null)
+    if (await repository.UpdateAsync(user) == false)
         return Results.NotFound();
-    userFromDb.UserName = user.UserName;
-    userFromDb.Password = user.Password;
-    userFromDb.Role = user.Role;
-    await db.SaveChangesAsync();
-    return Results.NoContent();
-});
-app.MapDelete("/users/{id}", async (int id, TasksDb db) =>
-{
-    var userFromDb = await db.Users.FindAsync(new object[] { id });
-    if (userFromDb == null)
-        return Results.NotFound();
-    db.Users.Remove(userFromDb);
-    await db.SaveChangesAsync();
+    await repository.SaveAsync();
     return Results.NoContent();
 });
 
-app.MapGet("/taskgroups", async (TasksDb db) => await db.TaskGroups.ToListAsync());
-app.MapGet("/taskgroups/{id}", async (int id, TasksDb db)
-    => await db.TaskGroups.FirstOrDefaultAsync(g => g.Id == id) is TaskGroup group
-    ? Results.Ok(group)
-    : Results.NotFound());
-app.MapPost("/taskgroups", async ([FromBody] TaskGroup group, TasksDb db) =>
+app.MapDelete("/users/{id}", async (IRepository<User> repository, int id) =>
 {
-    if (db.TaskGroups.Any(g => g.Id == group.Id || g.Name == group.Name))
-        throw new Exception("TaskGroup already exists");
-    db.TaskGroups.Add(group);
-    await db.SaveChangesAsync();
+    if (await repository.DeleteByIdAsync(id) == false)
+        return Results.NotFound();
+    await repository.SaveAsync();
+    return Results.NoContent();
+});
+
+// TaskGroups Api
+app.MapGet("/taskgroups", async (IRepository<TaskGroup> repository)
+    => Results.Ok(await repository.GetAllAsync()));
+
+app.MapGet("/taskgroups/{id}", async (IRepository<TaskGroup> repository, int id)
+    => Results.Ok(await repository.GetByIdAsync(id)));
+
+app.MapPost("/taskgroups", async (IRepository<TaskGroup> repository, [FromBody] TaskGroup group) =>
+{
+    if (await repository.InsertAsync(group) == false)
+        return Results.BadRequest();
+    await repository.SaveAsync();
     return Results.Created($"/taskgroups/{group.Id}", group);
 });
-app.MapPut("/taskgroups", async ([FromBody] TaskGroup group, TasksDb db) =>
+
+app.MapPut("/taskgroups", async (IRepository<TaskGroup> repository, [FromBody] TaskGroup group) =>
 {
-    var groupFromDb = await db.TaskGroups.FindAsync(new object[] { group.Id });
-    if (groupFromDb == null)
+    if (await repository.UpdateAsync(group) == false)
         return Results.NotFound();
-    groupFromDb.Name = group.Name;
-    groupFromDb.Description = group.Description;
-    await db.SaveChangesAsync();
-    return Results.NoContent();
-});
-app.MapDelete("/taskgroups/{id}", async (int id, TasksDb db) =>
-{
-    var groupFromDb = await db.TaskGroups.FindAsync(new object[] { id });
-    if (groupFromDb == null)
-        return Results.NotFound();
-    db.TaskGroups.Remove(groupFromDb);
-    await db.SaveChangesAsync();
+    await repository.SaveAsync();
     return Results.NoContent();
 });
 
-app.MapGet("/taskitems", async (TasksDb db) =>
+app.MapDelete("/taskgroups/{id}", async (IRepository<TaskGroup> repository, int id) =>
 {
-    JsonSerializerOptions options = new()
-    {
-        ReferenceHandler = ReferenceHandler.IgnoreCycles,
-        WriteIndented = true
-    };
-    return JsonSerializer.Serialize(await db.TaskItems.Include(i => i.Doer)
-        .Include(i => i.Group).ToListAsync(), options);
-});
-app.MapGet("/taskitems/{idUser}", async (int idUser, TasksDb db) => await db.TaskItems
-    .Where(i => i.Doer != null && i.Doer.Id == idUser).ToListAsync());
-app.MapGet("/taskitems/{idUser}/{num}", async (int idUser, int num, TasksDb db) =>
-{
-    var list = await db.TaskItems.Where(i => i.Doer != null && i.Doer.Id == idUser).ToListAsync();
-    if (list.Count >= num)
+    if (await repository.DeleteByIdAsync(id) == false)
         return Results.NotFound();
-    return Results.Ok(list[num]);
+    await repository.SaveAsync();
+    return Results.NoContent();
 });
-app.MapPost("/taskitems", async ([FromBody] TaskItem item, TasksDb db) =>
+
+// TaskItems Api
+JsonSerializerOptions options = new()
 {
-    if (db.TaskItems.Any(i => i.Id == item.Id))
-        throw new Exception("TaskItem already exists");
-    db.TaskItems.Add(item);
-    await db.SaveChangesAsync();
+    ReferenceHandler = ReferenceHandler.IgnoreCycles,
+    WriteIndented = true
+};
+app.MapGet("/taskitems", async (IRepository<TaskItem> repository) =>
+    Results.Ok(JsonSerializer.Serialize(await repository.GetAllAsync(), options)));
+
+app.MapGet("/taskitems/{idUser}", async (IRepository<TaskItem> repository, int idUser)
+    => Results.Ok(JsonSerializer.Serialize(await repository.GetByIdAsync(idUser), options)));
+
+app.MapGet("/taskitems/{idUser}/{num}", async (IRepository<TaskItem> repository, int idUser, int num) =>
+ {
+     var res = await repository.GetBySpecifiedIdAsync(idUser, num);
+     return res is TaskItem item ? Results.Ok(item) : Results.NotFound();
+ });
+
+app.MapPost("/taskitems", async (IRepository<TaskItem> repository, [FromBody] TaskItem item) =>
+{
+    if (await repository.InsertAsync(item) == false)
+        return Results.BadRequest();
+    await repository.SaveAsync();
     return Results.Created($"/taskitems/{item.Doer?.Id}", item);
 });
-app.MapPut("/taskitems", async ([FromBody] TaskItem item, TasksDb db) =>
+
+app.MapPut("/taskitems", async (IRepository<TaskItem> repository, [FromBody] TaskItem item) =>
 {
-    var taskItemFromDb = await db.TaskItems.FindAsync(new object[] { item.Id });
-    if (taskItemFromDb == null)
+    if (await repository.UpdateAsync(item) == false)
         return Results.NotFound();
-    taskItemFromDb.Name = item.Name;
-    taskItemFromDb.Description = item.Description;
-    taskItemFromDb.DoerId = item.DoerId;
-    taskItemFromDb.TaskGroupId = item.TaskGroupId;
-    taskItemFromDb.Begin = item.Begin;
-    taskItemFromDb.End = item.End;
-    taskItemFromDb.IsCompleted = item.IsCompleted;
-    taskItemFromDb.Priority = item.Priority;
-    await db.SaveChangesAsync();
+    await repository.SaveAsync();
     return Results.NoContent();
 });
-app.MapDelete("/taskitems/{idUser}/{num}", async (int idUser, int num, TasksDb db) =>
+
+app.MapDelete("/taskitems/{idUser}/{num}", async (IRepository<TaskItem> repository, int idUser, int num) =>
 {
-    var taskItemsFromDb = await db.TaskItems.Where(i => i.Doer != null && i.Doer.Id == idUser).ToListAsync();
-    if (taskItemsFromDb.Count >= num)
+    if (await repository.DeleteBySpecifiedIdAsync(idUser, num) == false)
         return Results.NotFound();
-    db.TaskItems.Remove(taskItemsFromDb[num]);
-    await db.SaveChangesAsync();
+    await repository.SaveAsync();
     return Results.NoContent();
 });
-app.MapDelete("/taskitems/{idUser}", async (int idUser, TasksDb db) =>
+
+app.MapDelete("/taskitems/{idUser}", async (IRepository<TaskItem> repository, int idUser) =>
 {
-    var taskItmesFromDb = await db.TaskItems.Where(x => x.Id == idUser).ToListAsync();
-    db.TaskItems.RemoveRange(taskItmesFromDb);
-    await db.SaveChangesAsync();
+    if (await repository.DeleteByIdAsync(idUser) == false)
+        return Results.NotFound();
+    await repository.SaveAsync();
     return Results.NoContent();
 });
 
