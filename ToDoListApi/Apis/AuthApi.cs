8cc19ab788c7f0d2c621c3228d4d7a4246351af7
Dtammie <dtammie@student.21-school.ru>
2024-07-09 17:09:39 +0700

    Complete refactoring the whole project

---
+++ ToDoListApi/Apis/AuthApi.cs
@@ -0,0 +1,101 @@
+﻿using ToDoListApi.Data;
+using ToDoListApi.Data.Auth;
+
+namespace ToDoListApi.Apis
+{
+    public class AuthApi : IMyApi
+    {
+        private WebApplication _app = null!;
+
+        public void RegisterEndPoints(WebApplication app)
+        {
+            _app = app;
+
+            app.MapPost("/login", Login)
+                .Accepts<UserDto>("application/json")
+                .Produces<string>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status401Unauthorized)
+                .WithSummary("Login")
+                .WithDescription("Sign in with username and password.")
+                .WithTags("Auth");
+
+            app.MapPost("/logout", Logout)
+                .ExcludeFromDescription();
+
+            app.MapPost("/register", Register)
+                .Accepts<UserDto>("application/json")
+                .Produces<string>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status400BadRequest)
+                .WithSummary("Register")
+                .WithDescription("Sign up for a new account.")
+                .WithTags("Auth");
+
+            app.MapPatch("/users/{id}/change-password", UpdateUserPassword)
+                .Accepts<User>("application/json")
+                .Produces(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status404NotFound)
+                .WithSummary("Update User Password")
+                .WithDescription("Not-admin user can change only their password. Admins can change all passwords.")
+                .WithTags("Updaters")
+                .RequireAuthorization("user-profile");
+
+            app.MapPatch("/users/{id}/change-role", UpdateUserRole)
+                .Accepts<User>("application/json")
+                .Produces(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status404NotFound)
+                .WithSummary("Update User Role")
+                .WithDescription("Change user's role (only by admins).")
+                .WithTags("Updaters")
+                .RequireAuthorization("user-profile");
+        }
+
+        [AllowAnonymous]
+        private async Task<IResult> Login(ITokenService tokenService, IAuth auth,
+            [FromBody] UserDto input)
+        {
+            var user = await auth.GetUser(input);
+            if (user is null)
+                return Results.Unauthorized();
+            var token = tokenService.BuildToken(_app.Configuration["Jwt:Key"]!,
+                _app.Configuration["Jwt:Issuer"]!, user);
+            return Results.Ok(token);
+        }
+
+        [Authorize]
+        private IResult Logout(ITokenService tokenService) =>
+                Results.NoContent();
+
+        [AllowAnonymous]
+        private async Task<IResult> Register(ITokenService tokenService,
+            IRepository<User> repository, IAuth auth, [FromBody] UserDto input)
+        {
+            if (!await auth.RegisterNewUser(input))
+                return Results.BadRequest("User already exists.");
+            await repository.SaveAsync();
+            var newUser = await auth.GetUser(input);
+            var token = tokenService.BuildToken(_app.Configuration["Jwt:Key"]!,
+                _app.Configuration["Jwt:Issuer"]!, newUser!);
+            return Results.Ok(token);
+        }
+
+        [Authorize]
+        private async Task<IResult> UpdateUserPassword(IRepository<User> repository,
+            IAuth auth, int id, string newPassword)
+        {
+            if (!await auth.UpdateUserPassword(id, newPassword))
+                return Results.NotFound("User not found.");
+            await repository.SaveAsync();
+            return Results.Ok($"Password was changed for user {id}.");
+        }
+
+        [Authorize(Roles = "admin")]
+        private async Task<IResult> UpdateUserRole(IRepository<User> repository, IAuth auth,
+            int id, string newRole)
+        {
+            if (!await auth.UpdateUserRole(id, newRole))
+                return Results.NotFound("User not found.");
+            await repository.SaveAsync();
+            return Results.Ok($"Role was changed for user {id}.");
+        }
+    }
+}
---
+++ ToDoListApi/Apis/TaskGroupsApi.cs
@@ -0,0 +1,88 @@
+﻿using ToDoListApi.Data;
+using ToDoListApi.Data.TaskGroups;
+
+namespace ToDoListApi.Apis
+{
+    public class TaskGroupsApi : IMyApi
+    {
+        public void RegisterEndPoints(WebApplication app)
+        {
+            app.MapGet("/taskgroups", GetAllTaskGroups)
+                .Produces<List<TaskGroup>>(StatusCodes.Status200OK)
+                .RequireAuthorization()
+                .WithSummary("Get All Task Groups")
+                .WithDescription("Get all task groups from the database.")
+                .WithTags("Getters");
+
+            app.MapGet("/taskgroups/{id}", GetTaskGroupById)
+                .Produces<TaskGroup>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status404NotFound)
+                .WithSummary("Get Task Group")
+                .WithDescription("Get task group by its id from the database.")
+                .WithTags("Getters");
+
+            app.MapPost("/taskgroups", CreateTaskGroup)
+                .Accepts<TaskGroup>("application/json")
+                .Produces<TaskGroup>(StatusCodes.Status201Created)
+                .Produces(StatusCodes.Status400BadRequest)
+                .WithSummary("Create New Task Group")
+                .WithDescription("Create new task group in the database (only for admins).")
+                .WithTags("Creators");
+
+            app.MapPut("/taskgroups", UpdateTaskGroup)
+                .Accepts<TaskGroup>("application/json")
+                .Produces(StatusCodes.Status204NoContent)
+                .Produces(StatusCodes.Status404NotFound)
+                .WithSummary("Update Task Group Fully")
+                .WithDescription("Update task group fullu in the database (only for admins).")
+                .WithTags("Updaters");
+
+            app.MapDelete("/taskgroups/{id}", DeleteTaskGroupById)
+                .Produces(StatusCodes.Status204NoContent)
+                .Produces(StatusCodes.Status404NotFound)
+                .WithSummary("Delete Task Group")
+                .WithDescription("Delete task group from the database (only for admins).")
+                .WithTags("Deleters");
+        }
+
+        [Authorize]
+        private async Task<IResult> GetAllTaskGroups(IRepository<TaskGroup> repository) =>
+            Results.Ok(await repository.GetAllAsync());
+
+        [Authorize]
+        private async Task<IResult> GetTaskGroupById(IRepository<TaskGroup> repository,
+            int id) => await repository.GetByIdAsync(id) is TaskGroup group
+            ? Results.Ok(group)
+            : Results.NotFound("Group not found.");
+
+        [Authorize(Roles = "admin")]
+        private async Task<IResult> CreateTaskGroup(IRepository<TaskGroup> repository,
+            [FromBody] TaskGroup group)
+        {
+            if (!await repository.InsertAsync(group))
+                return Results.BadRequest();
+            await repository.SaveAsync();
+            return Results.Created($"/taskgroups/{group.Id}", group);
+        }
+
+        [Authorize(Roles = "admin")]
+        private async Task<IResult> UpdateTaskGroup(IRepository<TaskGroup> repository,
+            [FromBody] TaskGroup group)
+        {
+            if (!await repository.UpdateAsync(group))
+                return Results.NotFound("Task group not found.");
+            await repository.SaveAsync();
+            return Results.NoContent();
+        }
+
+        [Authorize(Roles = "admin")]
+        private async Task<IResult> DeleteTaskGroupById(IRepository<TaskGroup> repository,
+            int id)
+        {
+            if (!await repository.DeleteByIdAsync(id))
+                return Results.NotFound("Task group not found.");
+            await repository.SaveAsync();
+            return Results.NoContent();
+        }
+    }
+}
